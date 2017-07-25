da5346d45829b98195fff52df2ffb86d4caed45c
Kyle <mcdougle.kyle@gmail.com>
2017-07-25 04:48:20 +0000

    football_project

---
+++ football_explorer/.~c9_invoke_ehfEU7.py
@@ -0,0 +1,19 @@
+import csv
+
+from .models import Player
+
+
+class FootballExplorer(object):
+    def __init__(self, csv_file_name):
+        self.csv_file_name = csv_file_name
+
+    def all(self):
+        '''
+        takes no parameters and should return an iterator that contains all the records from the csv file as Player objects.
+        '''
+        re
+        # raise NotImplementedError()
+
+    def search(self, country=None, year=None, age=None, position=None):
+        raise NotImplementedError()
+
