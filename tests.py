a1a16a5b49d86ef1cebb44e5d2150bc79addf0d3
Ping Chen <pxx_chen@yahoo.com>
2017-07-26 05:04:47 +0000

    SecondTry

--- tests.py
+++ tests.py
@@ -6,8 +6,8 @@ from football_explorer import FootballExplorer
 
 def test_cant_search_without_params():
     explorer = FootballExplorer(csv_file_name='test_data.csv')
-    with pytest.raises(ValueError): 
-        explorer.search()
+    with pytest.raises(ValueError):
+        next(explorer.search())
 
 
 def test_explorer_returns_all():
