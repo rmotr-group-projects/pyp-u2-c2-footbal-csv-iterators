b816a8efc864e5e6793a2cf7a803134f21a14357
ry-werth <ryan@draftboard.com>
2017-07-27 10:24:25 -0700

    Update explorer.py

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -2,13 +2,75 @@ import csv
 
 from .models import Player
 
+class SearchIterator(object):
+    def __init__(self, csv_file_name, country= None, year = None, age = None, position = None):
+        self.country = country
+        self.year = year
+        self.age = age
+        self.position = position
+        self.csv_file_name = csv_file_name
+        self.OpenFile = open(self.csv_file_name)
+        self.fileReader = csv.reader(self.OpenFile)
+        
+    def isMatch(self, player):
+        if (self.country and getattr(player, 'country') != self.country):
+            return False
+            
+        if (self.year and getattr(player, 'year') != self.year):
+            return False
+            
+        if (self.age and getattr(player, 'age') != self.age):
+            return False
+            
+        if (self.position and getattr(player, 'position') != self.position):
+            return False
+            
+        return True
+        
+    def __iter__(self):
+        return self
+        
+    def __next__(self):
+        if self.OpenFile.closed:
+            raise StopIteration()
+        try:
+            line = (next(self.fileReader))
+            self.player = Player(*line)
+            if self.isMatch(self.player):
+                return self.player
+            return self.next()
+                
+        except StopIteration:
+            self.OpenFile.close()
+            raise StopIteration()
+            
+    next = __next__
+        
 
 class FootballExplorer(object):
     def __init__(self, csv_file_name):
         self.csv_file_name = csv_file_name
 
     def all(self):
-        raise NotImplementedError()
+        self.OpenFile = open(self.csv_file_name)
+        self.fileReader = csv.reader(self.OpenFile)
+        self.PlayerList = []
+        self.moreLines = True
+        while self.moreLines:
+            try:
+                line = (next(self.fileReader))
+                self.player = Player(*line)
+                self.PlayerList.append(self.player)
+            except StopIteration:
+                self.OpenFile.close()
+                self.moreLines = False
+        return(self.PlayerList)
+                
+            
+            
 
     def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
+        if not any([country, year, age, position]):
+            raise ValueError()
+        return SearchIterator(
+            self.csv_file_name, country, year, age, position)
