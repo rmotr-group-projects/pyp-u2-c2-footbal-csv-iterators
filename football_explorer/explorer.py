cbaad23ecbf5df53fef3e30306ddb54457b6965a
Pamela <pb2001@gmail.com>
2017-07-27 04:40:09 +0000

    I have an error for test 4 (country and year) but it is very weird and I cant figure it out. 1) If I dont include a next(self) function (NOT __next__(self)) I get an error that it doesnt recognize my classes as iterators; 2) When I do include the next(self) functions, it is running that next function when it is iterating though (see where the print statement is for the FilteredPlayerIterator in the next function); 3) It also passes the first three tests, but on the last test (country and year), though the search method seems to be functioning correctly (see the print statement I left in there - it is ending on Oscar), for some reason it is returning None, not a Player object

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -2,13 +2,77 @@ import csv
 
 from .models import Player
 
-
 class FootballExplorer(object):
     def __init__(self, csv_file_name):
         self.csv_file_name = csv_file_name
 
     def all(self):
-        raise NotImplementedError()
+        return PlayerIterator(self.csv_file_name)
 
     def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
+        arglist = [country, year, age, position]
+        if arglist.count(arglist[0]) == len(arglist):
+            raise ValueError()
+        else:
+            return FilteredPlayerIterator(self.csv_file_name, arglist)
+
+
+class PlayerIterator(object):
+    def __init__(self, csv_fname):
+        self.reader = csv.reader(open(csv_fname))
+    
+    def __iter__(self):
+        return(self)
+
+    def __next__(self):
+        try:
+            yay = next(self.reader)
+        except StopIteration:
+            raise StopIteration()
+        return Player(*yay)
+
+    def next(self):
+        try:
+            yay = next(self.reader)
+        except StopIteration:
+            raise StopIteration()
+        return Player(*yay)
+        
+
+class FilteredPlayerIterator(PlayerIterator):
+    def __init__(self, csv_fname, arglist):
+        self.reader = csv.reader(open(csv_fname))
+        self.arglist = arglist
+
+    def __next__(self):
+        try:
+            yay = next(self.reader)
+        except StopIteration:
+            raise StopIteration()
+        next_player = Player(*yay)
+        player_args = [next_player.country, next_player.year, next_player.date_of_birth-next_player.year, next_player.position]
+        conditions = [i for i, e in enumerate(self.arglist) if e != None]
+        good = False
+        for j in range(0,len(conditions)):
+            good = (self.arglist[j] == player_args[j])
+        if good == True:
+            return next_player
+        else:
+            self.__next__()
+        
+    def next(self):
+        try:
+            yay = next(self.reader)
+        except StopIteration:
+            raise StopIteration()
+        next_player = Player(*yay)
+        player_args = [next_player.country, next_player.year, next_player.date_of_birth, next_player.position]
+        conditions = [i for i, e in enumerate(self.arglist) if e != None]
+        good = False
+        for j in conditions:
+            good = (self.arglist[j] == player_args[j])
+        if good == True:
+            print next_player.name          # the variable I am returning is a Player object
+            return next_player              # but in tests.py, it says it is a NoneType object
+        else:
+            self.next()
