a9dfbeb50f749862a05185d13e3e8ebb6501b108
Devan Govender <devakumarangovender@gmail.com>
2017-09-12 22:43:59 +0000

    project refactored with generator expressions

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -1,5 +1,4 @@
 import csv
-
 from .models import Player
 
 
@@ -8,7 +7,18 @@ class FootballExplorer(object):
         self.csv_file_name = csv_file_name
 
     def all(self):
-        raise NotImplementedError()
+        with open(self.csv_file_name) as fname:
+            self.fname_csv = csv.reader(fname)
+            self.record_list = (Player(*line) for line in self.fname_csv)
+            for player in self.record_list:
+                yield player
 
-    def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
+    def search(self, **kwargs):
+        if not any(kwargs.items()):
+            raise ValueError
+        '''
+        for player in self.all():
+            if all([getattr(player, key) == val for key, val in kwargs.items()]):
+                yield player
+        '''
+        return (player for player in self.all() if all([getattr(player, key,'') == val for key, val in kwargs.items()]))
