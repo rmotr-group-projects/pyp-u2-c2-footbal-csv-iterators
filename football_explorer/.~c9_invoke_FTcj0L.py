48147cb0ea5341b6488c71679eed594d7874f951
Sara Amin <saraaamin@gmail.com>
2017-09-13 12:54:57 +0000

    submitting the project after successfully running all test cases

---
+++ football_explorer/.~c9_invoke_FTcj0L.py
@@ -0,0 +1,25 @@
+import csv
+
+from .models import Player
+
+
+class FootballExplorer(object):
+    def __init__(self, csv_file_name):
+        self.csv_file_name = csv_file_name
+
+    def all(self):
+        playersList = []
+        with open(self.csv_file_name, 'r') as fp:
+            for row in fp:
+                playerInfo = row.split(',')
+                playerObj = Player(number = playerInfo[0], position = playerInfo[1], 
+                    name = playerInfo[2], date_of_birth = playerInfo[3], caps = playerInfo[4], 
+                    club = playerInfo[5], country = playerInfo[6], club_country = playerInfo[7], year = playerInfo[8])
+                playersList.append(playerObj) 
+                print playerInfo
+        return playersList                            
+        
+
+    def search(self, country=None, year=None, age=None, position=None):
+        if country is None and year is None and age is None and position is None:
+            r
--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -6,9 +6,39 @@ from .models import Player
 class FootballExplorer(object):
     def __init__(self, csv_file_name):
         self.csv_file_name = csv_file_name
-
+        self.playersList = self.all()
+        
     def all(self):
-        raise NotImplementedError()
-
+        self.playersList = []
+        with open(self.csv_file_name, 'r') as fp:
+            for row in fp:
+                playerInfo = row.split(',')
+                playerObj = Player(number = playerInfo[0], position = playerInfo[1], 
+                    name = playerInfo[2], date_of_birth = playerInfo[3], caps = playerInfo[4], 
+                    club = playerInfo[5], country = playerInfo[6], club_country = playerInfo[7], year = playerInfo[8])
+                self.playersList.append(playerObj) 
+                
+        return self.playersList                            
+        
     def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
+        if country is None and year is None and age is None and position is None:
+            raise ValueError
+        else:
+            termsList = {}
+            if country is not None:
+                termsList['country'] = country
+            
+            if year is not None:
+                termsList['year'] = year
+            
+            if age is not None:
+                termsList['age'] = age
+            
+            if position is not None:
+                termsList['position'] = position
+            
+            
+            for playerObj in self.playersList:
+                if all([getattr(playerObj, key) == termsList[key] for key in termsList]):
+                    yield playerObj
+        
