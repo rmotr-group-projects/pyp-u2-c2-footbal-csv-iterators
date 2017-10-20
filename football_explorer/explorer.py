3476e0bd3ba548c3f0947418d299f8b141053c1b
Alicia Stillwell <alicia.stillwell@gmail.com>
2017-10-20 17:17:38 +0000

    Added search functionality

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -2,31 +2,34 @@ import csv
 
 from .models import Player
 
-#It may make things easier to create a base iterator class for returning all records and then have a child class inherit from that for handling filtering search results.
-
-'''
-with open('squads.csv') as fp:
-    reader = csv.reader(fp)
-    for line in reader:
-        player = Player(*line)
-        print(player)
-'''
-
 class FootballExplorer(object):
     def __init__(self, csv_file_name):
         self.csv_file_name = csv_file_name
 
     def all(self):
-        new_player = lambda x: Player(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8])
-        all_footballers = []
-        
-        f = open(self.csv_file_name)
-        footballers = csv.reader(f)
-        for footballer in footballers:
-            all_footballers.append(new_player(footballer))
+        with open(self.csv_file_name) as f:
+            footballers = csv.reader(f)
+            all_footballers = []
+            for footballer in footballers:
+                all_footballers.append(Player(*footballer))
         return all_footballers
 
     def search(self, country=None, year=None, age=None, position=None):
-        # If none of these parameters are given it should raise a ValueError
-        # return an iterator that contains all the records from the csv file that match the given parameters as Player objects
-        raise NotImplementedError()
+        search_args = locals()
+        player_idx = {
+          'country': 6,
+          'year':8,
+          'age':3,
+          'position':1
+        }
+        filter_dict = {player_idx[key]: search_args[key] for key in player_idx if search_args[key] is not None}
+        
+        if not filter_dict:
+            raise ValueError()
+        with open(self.csv_file_name) as f:
+            footballers = csv.reader(f)
+            matched_footballers = []
+            for footballer in footballers:
+                if all([footballer[f_key] == str(filter_dict[f_key]) for f_key in filter_dict]):
+                    matched_footballers.append(Player(*footballer))
+        return matched_footballers
