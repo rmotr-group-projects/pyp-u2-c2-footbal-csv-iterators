87ae45a858831434ae5b27802ec5d4efb08d8143
Erika Boren <eboren@gmail.com>
2017-10-20 23:18:25 +0000

    add all method to return all footballers from csv file

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -8,7 +8,32 @@ class FootballExplorer(object):
         self.csv_file_name = csv_file_name
 
     def all(self):
-        raise NotImplementedError()
+        with open(self.csv_file_name, 'r') as f:
+            all_players = []
+            csvreader = csv.reader(f)
+            for item in csvreader:
+                all_players.append(Player(*item))
+        return (all_players)    
 
     def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
+       if not any([country, year, age, position]):
+             raise ValueError()
+             
+       search_dict = {'country': country, 'year': year, 'age': age, 'position': position } 
+       
+    #   for k,v in search_dict.items():
+    #      if v is not None:
+    #       with open(self.csv_file_name, 'r') as f:
+    #          csvreader = csv.reader(f)
+    #          fields = csvreader.next()
+    #          match = []
+    #          for item in csvreader:
+    #              if item == 
+   
+   
+    # def __iter__(self):
+    #     return self
+        
+    # def __next__(self):
+    #     pass
+            
