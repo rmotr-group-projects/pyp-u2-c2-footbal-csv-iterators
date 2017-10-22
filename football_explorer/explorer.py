09d444d9ddb44f690e5149b9bde96b5d0c2358ba
benny1979 <aldridge.b@googlemail.com>
2017-10-22 09:24:58 +0000

    working search

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -2,13 +2,65 @@ import csv
 
 from .models import Player
 
-
 class FootballExplorer(object):
     def __init__(self, csv_file_name):
         self.csv_file_name = csv_file_name
+        self.file = None
+        self.reader = None
+        #self.country = None
+        #self.age = None
+        #self.year = None
+        #self.position = None
+        
+    def __iter__(self):
+        #if not self.file:
+        self.file = open(self.csv_file_name)
+        self.reader = csv.reader(self.file)
+        return self
+    '''        
+    def __next__(self):
+        try:
+            line = next(self.reader)
+            player = Player(*line)
+            return player
+        except:
+            raise StopIteration()
 
     def all(self):
-        raise NotImplementedError()
+        return self
 
+    next = __next__
+
+class FootballSearch(FootballExplorer):
+    def __init__(self, csv_file_name):
+        super().__init__(csv_file_name)
+    '''
+    def __next__(self):
+        try:
+            line = next(self.reader)
+            player = Player(*line)
+            if self.check_player(player):
+                return player
+            return next(self)
+        except:
+            raise StopIteration()
+        
+    next =__next__
+    
+    def check_player(self, other):
+        for k, v in self.search_params.items():
+            if getattr(other, k) != v:
+                return False
+        return True
+    
     def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
+        if all([country==None, year==None, age==None, position==None]):
+        #alternatively
+        #if not any([country, year, age, position]):
+            raise ValueError()
+        self.country = country
+        self.year = year
+        self.age = age
+        self.position = position
+        self.search_params = {k: v for k, v in dict(locals()).items() if k!='self' and v != None}
+        return self
