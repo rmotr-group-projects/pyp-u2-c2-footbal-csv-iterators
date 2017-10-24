24e59b876f46c1174a97c86b9e683345e3fa8f39
Sungo Afonso <afsungo@gmail.com>
2017-10-24 22:22:31 +0000

    My-football-csv-iterators

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -8,7 +8,56 @@ class FootballExplorer(object):
         self.csv_file_name = csv_file_name
 
     def all(self):
-        raise NotImplementedError()
+        result = []
+        
+        lines = self.line_player
+        
+        try:
+            for line in lines:
+                result += [ Player(*line) ]
+        except StopIteration:
+            raise
+        return result
+    
+    '''
+        This function return a generator with file lines
+    '''
+    @property
+    def line_player(self):
+        with open(self.csv_file_name) as fp:
+            reader = csv.reader(fp)
+
+            for line in reader:
+                yield line
 
     def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
+        result = []
+        
+        if country == None and year == None and age == None and position == None:
+            raise ValueError()
+        
+        lines = self.line_player
+        
+        while True:
+            try:
+                line = next(lines) # Return the next item into iterator, using generator!
+                player = Player(*line)
+            
+                if country == player.country and not year:
+                    result += [ player ]
+                
+                if country == player.country and year == player.year:
+                    result += [ player ]
+            except StopIteration:
+                break
+
+        return result
+    
+    
+    
+    
+    
+    
+    
+    
+    
