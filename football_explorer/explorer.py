9af52ff5c880c9edd99e09dd1a71167ae3e42a5c
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-10-25 16:07:52 +0000

    a little reformatting comments to look better

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -7,6 +7,7 @@ class FootballExplorer(object):
         self.csv_file_name = csv_file_name
 
     def all(self):
+
         # Using a generator to go through the file
         with open(self.csv_file_name) as fp:
             reader = csv.reader(fp)
@@ -16,17 +17,20 @@ class FootballExplorer(object):
     def search(self, country=None, year=None, age=None, position=None):
         if not (country or year or age or position):
             raise ValueError()
+
         # This generator calls the all() generator and searches through
         # the players recieved through that.
         for player in self.all():
+
             # Using this large if statement we can search the parameters given
             # whether it is one or all of them
+            # date_of_birth includes age is combined with it so we split the
+            # string at age which is in the last few characters [-3:-1]
             if (country and player.country != country or
                     year and player.year != year or
-                    # Because they only have date of birth and age we split the
-                    # string at age which is in the last few characters
                     age and int(player.date_of_birth[-3:-1]) != age or
                     position and player.position != position):
+
                 # Using continue to skip the yield at the end of the method
                 continue
             yield player
