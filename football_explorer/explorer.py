5fbe4c4330d8c32959762b9fb8315fd096f271f5
Khristopher Chireno <kcmastermind2005@gmail.com>
2017-10-25 13:35:19 +0000

    fixed age search and added comments

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -7,7 +7,7 @@ class FootballExplorer(object):
         self.csv_file_name = csv_file_name
 
     def all(self):
-    # Using a generator to go through the file
+        # Using a generator to go through the file
         with open(self.csv_file_name) as fp:
             reader = csv.reader(fp)
             for line in reader:
@@ -16,10 +16,17 @@ class FootballExplorer(object):
     def search(self, country=None, year=None, age=None, position=None):
         if not (country or year or age or position):
             raise ValueError()
+        # This generator calls the all() generator and searches through
+        # the players recieved through that.
         for player in self.all():
+            # Using this large if statement we can search the parameters given
+            # whether it is one or all of them
             if (country and player.country != country or
                     year and player.year != year or
-                    age and player.age != age or
+                    # Because they only have date of birth and age we split the
+                    # string at age which is in the last few characters
+                    age and int(player.date_of_birth[-3:-1]) != age or
                     position and player.position != position):
+                # Using continue to skip the yield at the end of the method
                 continue
             yield player
