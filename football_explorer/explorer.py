96ab1df63c93989e8830be96cfc3f6bc9fcb972f
Alicia Stillwell <alicia.stillwell@gmail.com>
2017-10-26 20:10:32 +0000

    Updated search to use all

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -1,5 +1,4 @@
 import csv
-
 from .models import Player
 
 class FootballExplorer(object):
@@ -9,27 +8,21 @@ class FootballExplorer(object):
     def all(self):
         with open(self.csv_file_name) as f:
             footballers = csv.reader(f)
-            all_footballers = []
             for footballer in footballers:
-                all_footballers.append(Player(*footballer))
-        return all_footballers
+                yield Player(*footballer)
 
     def search(self, country=None, year=None, age=None, position=None):
         search_args = locals()
-        player_idx = {
-          'country': 6,
-          'year':8,
-          'age':3,
-          'position':1
+        player_attr = {
+            'country': 'country',
+            'year': 'year',
+            'age': 'date_of_birth',
+            'position': 'position'
         }
-        filter_dict = {player_idx[key]: search_args[key] for key in player_idx if search_args[key] is not None}
+        filter_dict = {player_attr[key]: search_args[key] for key in player_attr if search_args[key] is not None}
         
         if not filter_dict:
             raise ValueError()
-        with open(self.csv_file_name) as f:
-            footballers = csv.reader(f)
-            matched_footballers = []
-            for footballer in footballers:
-                if all([footballer[f_key] == str(filter_dict[f_key]) for f_key in filter_dict]):
-                    matched_footballers.append(Player(*footballer))
-        return matched_footballers
+        for footballer in self.all():
+            if all([getattr(footballer, arg) == filter_dict[arg] for arg in filter_dict]):
+                yield footballer
