ca3d2be17bce7630b73f6c76c25dec14f4c7527b
Kelli Blalock <kelli.blalock2@gmail.com>
2017-10-26 00:57:05 +0000

    Pass  test_explorer_returns_all

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -1,6 +1,26 @@
 import csv
 
-from .models import Player
+from models import Player
+
+
+class FootballIterator(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.fp = open(self.filename)
+        self.reader = csv.reader(self.fp)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            player = Player(*next(self.reader))
+            return player
+        except:
+            self.fp.close()
+            raise StopIteration()
+
+    next = __next__
 
 
 class FootballExplorer(object):
@@ -8,7 +28,10 @@ class FootballExplorer(object):
         self.csv_file_name = csv_file_name
 
     def all(self):
-        raise NotImplementedError()
+        return FootballIterator(self.csv_file_name)
 
     def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
+        if (country is None and year is None and age is None and
+                position is None):
+                    raise ValueError('Must provide country, year, age, or '
+                                     'position')
