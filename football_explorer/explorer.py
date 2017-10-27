5dfb35f7a3ab9864d354ca20b2ba1ea46eac24ae
laurauzcategui <laura.uzcateguij@gmail.com>
2017-10-28 00:59:58 +0100

    Iterator Futbol Project

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -6,32 +6,73 @@ from .models import Player
 class FootballExplorer(object):
     def __init__(self, csv_file_name):
         self.csv_file_name = csv_file_name
+
+    def all(self):
+        return PersonsIterator(self.csv_file_name)
+
+    def search(self, country=None, year=None, age=None, position=None):
+        if not any([country,year,age,position]):
+            raise ValueError
+        return SearchIterator(self.csv_file_name,country,year,age,position)
+
+class PersonsIterator(object):
+    def __init__(self,csv_file_name):
         self.players = []
         self.idx = 0
+        self.csv_file_name = csv_file_name
 
     def __iter__(self):
-        self.read_csv()
+        self.players = []
         self.idx = 0
-        return self.players
+        self.read_csv()
+        return self
 
     def read_csv(self):
-        with open(self.csv_file_name, newline='') as csvfile:
+        with open(self.csv_file_name) as csvfile:
             fields = ["number","position","name","date_of_birth","caps","club","country","club_country","year"]
-            reader = csv.reader(csvfile,fieldnames=fields,delimiter=',')
+            reader = csv.DictReader(csvfile,fieldnames=fields,delimiter=',')
             for row in reader:
-                new_player = Player(row.number,row.position,row.name,row.date_of_birth,row.caps,row.club,row.country,row.club_country,row.year)
+                new_player = Player(row['number'],
+                                    row['position'],
+                                    row['name'],
+                                    row['date_of_birth'],
+                                    row['caps'],
+                                    row['club'],
+                                    row['country'],
+                                    row['club_country'],
+                                    row['year'])
                 self.players.append(new_player)
 
-    def __next__():
-        if self.idx + 1 > len(players):
+    def __next__(self):
+        if self.idx + 1 > len(self.players):
             raise StopIteration
         player = self.players[self.idx]
         self.idx += 1
         return player
 
+    next = __next__
 
-    def all(self):
-        return self.__iter__()
+class SearchIterator(PersonsIterator):
+    def __init__(self, filename, country=None, year=None, age=None, position=None):
+        self.filters  =  {
+            'country' : country,
+            'year' : year,
+            'age' : age,
+            'position' : position
+        }
 
-    def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
+        super(SearchIterator, self).__init__(filename)
+
+    def search_by_terms(self,player):
+        for key, value in self.filters.items():
+            if value and getattr(player, key) != value:
+                return False
+        return True
+
+    def __next__(self):
+        player = super(SearchIterator, self).__next__()
+        if self.search_by_terms(player):
+            return player
+        return next(self)
+
+    next = __next__
