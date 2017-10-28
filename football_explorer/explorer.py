2113f97115d8254fdc66f9322dab5382f234189c
Kelli Blalock <kelli.blalock2@gmail.com>
2017-10-28 00:46:40 +0000

    Pass test_search_by_country)

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -23,6 +23,28 @@ class FootballIterator(object):
     next = __next__
 
 
+class SearchIterator(FootballIterator):
+    def __init__(self, filename, country=None, year=None, age=None,
+                 position=None):
+        super(SearchIterator, self).__init__(filename)
+        self.country = country
+        self.year = year
+        self.age = age
+        self.position = position
+
+    def __next__(self):
+        try:
+            player = Player(*next(self.reader))
+            while self.country != player.country:
+                player = Player(*next(self.reader))
+            return player
+        except:
+            self.fp.close()
+            raise StopIteration()
+
+    next = __next__
+
+
 class FootballExplorer(object):
     def __init__(self, csv_file_name):
         self.csv_file_name = csv_file_name
@@ -34,4 +56,5 @@ class FootballExplorer(object):
         if (country is None and year is None and age is None and
                 position is None):
                     raise ValueError('Must provide country, year, age, or '
-                                     'position')
+                                     'position.')
+        return SearchIterator(self.csv_file_name, country, year, age, position)
