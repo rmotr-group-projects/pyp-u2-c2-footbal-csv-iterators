15b37d7d8bfb6e855a4236afab796506cde58d96
cheeyim <cheeyim@gmail.com>
2017-10-30 15:02:54 +0000

    convert for loop to PlayerIterator

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -2,6 +2,25 @@ import csv
 
 from .models import Player
 
+class PlayerIterator(object):
+    
+    def __init__(self, players):
+        self.players = players
+        self.index = 0
+    
+    def __iter__(self):
+        return self
+        
+    def __next__(self):
+        if self.index >= len(self.players):
+            raise StopIteration()
+            
+        elem = self.players[self.index]
+        self.index += 1
+        return elem
+            
+    next = __next__
+    
 
 class FootballExplorer(object):
     def __init__(self, csv_file_name):
@@ -9,27 +28,25 @@ class FootballExplorer(object):
         self.players = self.load_file()
 
     def load_file(self):
-        f = open(self.csv_file_name, 'r')
         players = []
-        for line in f:
-            items = line.split(',')
-            player = Player(items[0], items[1], items[2], items[3], 
-                            items[4], items[5], items[6], items[7], 
-                            items[8])
-            players.append(player)
-            
-        f.close()
+        with open(self.csv_file_name, 'r') as f:
+            for line in f:
+                items = line.split(',')
+                player = Player(items[0], items[1], items[2], items[3], 
+                                items[4], items[5], items[6], items[7], 
+                                items[8])
+                players.append(player)
         
         return players
     
     def all(self):
-        return self.players
+        return PlayerIterator(self.players)
 
     def search(self, country=None, year=None, age=None, position=None):
         if (country or year or age or position) == None:
             raise ValueError()
-           
-        return [player for player in self.players 
+            
+        return [player for player in self.all()
                 if (country == player.country or not country) and
                    (year == player.year or not year) and 
                    (age == player.date_of_birth[-3:-1] or not age) and
