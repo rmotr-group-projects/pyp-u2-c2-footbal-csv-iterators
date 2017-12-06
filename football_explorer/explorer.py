a77d90ca98759f3cff6fa250f7336f226e4187eb
unoor <usman.noor127@hotmail.com>
2017-12-06 23:17:29 +0000

    Files Working Fine

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -6,9 +6,22 @@ from .models import Player
 class FootballExplorer(object):
     def __init__(self, csv_file_name):
         self.csv_file_name = csv_file_name
-
+        
     def all(self):
-        raise NotImplementedError()
+        with open(self.csv_file_name) as fp:
+            reader = csv.reader(fp)
+            for line in reader:
+                player = Player(*line)
+                yield player
+        
+        
+    def search(self, **kwargs):
+        if len(kwargs) == 0:
+            raise ValueError
+        with open(self.csv_file_name) as fp:
+            reader = csv.reader(fp)
+            for line in reader:
+                player = Player(*line)
+                if all([getattr(player, key) == kwargs[key] for key, value in kwargs.items()]):
+                    yield player
 
-    def search(self, country=None, year=None, age=None, position=None):
-        raise NotImplementedError()
