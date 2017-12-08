251042d5ea4a756ca40b3fea4aa0ef437ef5ec2f
Palermo Penano <palermo.penano@mail.utoronto.ca>
2017-12-08 16:10:53 -0500

    modify code to follow structure of solution using generators

--- football_explorer/explorer.py
+++ football_explorer/explorer.py
@@ -17,31 +17,32 @@ class FootballExplorer(object):
             for line in reader:
                 yield Player(*line)
 
+    def __search_match(self, player, country=None, year=None, age=None, position=None):
+
+        search_criteria = [country, year, age, position]
+
+        player_attribute = [player.country,
+                            player.year,
+                            player.date_of_birth,
+                            player.position]
+
+        # check if criteria equal attribute only for criteria that were provided
+        search_restriction = [(criteria == attribute)
+                              for criteria, attribute in zip(search_criteria, player_attribute)
+                              if criteria]
+
+        return all(search_restriction)
+
     def search(self, country=None, year=None, age=None, position=None):
         """
         Yields the Player object the matches search criteria
         """
 
-        search_criteria = [country, year, age, position]
-
-        if not any(search_criteria):
+        if not any([country, year, age, position]):
             raise ValueError
 
-        with open(self.csv_file_name) as fp:
-            reader = csv.reader(fp)
-            for line in reader:
+        for player in self.all():
 
-                player = Player(*line)
+            if self.__search_match(player, country, year, age, position):
 
-                player_attribute = [player.country,
-                                    player.year,
-                                    player.date_of_birth,
-                                    player.position]
-
-                # check if criteria equal attribute only for criteria that were provided
-                search_restriction = [(criteria == attribute)
-                                      for criteria, attribute in zip(search_criteria, player_attribute)
-                                      if criteria]
-
-                if all(search_restriction):
-                    yield player
+                yield player
